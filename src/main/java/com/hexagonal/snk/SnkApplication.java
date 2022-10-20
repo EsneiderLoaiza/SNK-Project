ab5b1c23cc6684b79e7800f81e05fc888044eea2
esneider-ies- <david.loaiza@iesonline.com.co>
2022-10-20 13:54:23 -0500

    first commit

---
+++ src/main/java/com/hexagonal/snk/SnkApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.snk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/snk/SnkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.snk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
